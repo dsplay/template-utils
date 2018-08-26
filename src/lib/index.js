653fbfa4322380a413dccbe131d52cf146b53ae4
pcmnac <pcmnac@gmail.com>
2018-08-26 09:58:47 -0300

    refactoring

--- src/lib/index.js
+++ src/lib/index.js
@@ -5,3 +5,12 @@ export {
     template,
     config,
 }
+
+
+export const tval = template.val;
+
+export const tbval = template.bval;
+
+export const tival = template.ival;
+
+export const tfval = template.fval;
--- src/lib/template.js
+++ src/lib/template.js
@@ -1,9 +1,9 @@
 const template = window.dsplay_template || window.template || {};
 
-export const tval = (key, defaultValue) => template[key] !== undefined && template[key] !== null ? template[key] : defaultValue;
+export const val = (key, defaultValue) => template[key] !== undefined && template[key] !== null ? template[key] : defaultValue;
 
-export const tbval = (key, defaultValue) => tval(key, defaultValue ? 'true' : 'false').toLowerCase() === 'true';
+export const bval = (key, defaultValue) => val(key, defaultValue ? 'true' : 'false').toLowerCase() === 'true';
 
-export const ibval = (key, defaultValue) => parseInt(tval(key, defaultValue));
+export const ival = (key, defaultValue) => parseInt(val(key, defaultValue));
 
-export const fbval = (key, defaultValue) => parseFloat(tval(key, defaultValue));
+export const fval = (key, defaultValue) => parseFloat(val(key, defaultValue));
