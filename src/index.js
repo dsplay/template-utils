621b709303cbc361e16cfd47154a4218bb6b4187
pcmnac <pcmnac@gmail.com>
2019-06-23 11:36:35 +0100

    ⬆️ fixing npm audit issues

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import { render } from "react-dom";
 
 const App = () => (
-  <div style={{ width: 640, margin: "15px auto" }}>
     <h1>Hello React</h1>
-  </div>
 );
 
 render(<App />, document.getElementById("root"));
