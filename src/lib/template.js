a7c68d4bd0c265e489f5e7aa0386f4b0ed65981f
Paulo Coutinho <pcmnac@gmail.com>
2020-08-01 17:40:07 +0100

    :sparkles: adding supports to boolean in dsplay-data

--- src/lib/template.js
+++ src/lib/template.js
@@ -3,8 +3,10 @@ const template = window.dsplay_template || window.template || {};
 export const val = (key, defaultValue) => template[key] ? template[key] : defaultValue;
 
 export const bval = (key, defaultValue) => {
-    const stringVal = val(key, defaultValue ? 'true' : 'false').toLowerCase();
-    return stringVal === 'true' || stringVal === '1';
+  let stringVal = val(key, defaultValue ? 'true' : 'false');
+  if (typeof(stringVal) === 'boolean') return stringVal;
+  stringVal = stringVal.toLowerCase();
+  return stringVal === 'true' || stringVal === '1';
 }
 
 export const ival = (key, defaultValue) => parseInt(val(key, defaultValue));
